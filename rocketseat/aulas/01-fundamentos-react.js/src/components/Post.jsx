efd7caed8edaaaf37240c6a130cfe47b1c8825a3
valtervilmerson <valter.vilmerson@gmail.com>
2025-01-19 18:12:25 +0000

    completo até ´sessão 2 - estilização do post´"

---
+++ rocketseat/aulas/01-fundamentos-react.js/src/components/Post.jsx
@@ -0,0 +1,43 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/valtervilmerson.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Valter Vilmerson</strong>
+            <span>Procrastinador</span>
+          </div>
+        </div>
+
+        <time
+          title="19 de janeiro de 2025 às 11:55"
+          dateTime="2025-01-19 11:55:00"
+        >
+          Publicado há 1 Hora
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifólio. É um projeto que
+          fiz no NLW Return, evento da Rocketseat. O nome do projeto é
+          DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
--- rocketseat/aulas/01-fundamentos-react.js/src/components/Sidebar.jsx
+++ rocketseat/aulas/01-fundamentos-react.js/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "@phosphor-icons/react";
+
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -9,12 +11,20 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/28738015?v=4"
+        />
+
         <strong>Valter Vilmerson</strong>
         <span>Procrastinador</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
